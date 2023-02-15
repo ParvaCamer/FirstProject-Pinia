db8917c7b305ec5bf6113e48a5da6a68e240265f
ParvaCamer <64281030+ParvaCamer@users.noreply.github.com>
2023-02-15 19:37:00 +0100

    first day

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <main>
+    <header>
+      <img src="./assets/pinia-logo-svg.svg" alt="pinia logo">
+      <h1>Pinia Tasks</h1>
+    </header>
+
+    <div class="task-list">
+      <div v-for="task in taskStore.tasks" :key="task.id">
+        <TaskDetails :task="task"/>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script>
+import { useTaskStore } from './stores/TaskStore';
+import TaskDetails from './components/TaskDetails.vue';
+export default {
+  components: {
+    TaskDetails
+  },
+  setup() {
+    const taskStore = useTaskStore()
+    return { taskStore }
+  }
+}
+</script>

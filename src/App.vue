2174f5e3058687d20cea437575118288e6de5b92
ParvaCamer <64281030+ParvaCamer@users.noreply.github.com>
2023-03-04 19:58:10 +0100

    getters

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,20 @@
       <h1>Pinia Tasks</h1>
     </header>
 
-    <div class="task-list">
+    <nav class="filter">
+      <button @click="filter = 'all'">All tasks</button>
+      <button @click="filter = 'favs'">Fav tasks</button>
+    </nav>
+
+    <div class="task-list" v-if="filter === 'all'">
+      <p>You have {{ taskStore.totalCount }} tasks left to do</p>
       <div v-for="task in taskStore.tasks" :key="task.id">
+        <TaskDetails :task="task"/>
+      </div>
+    </div>
+    <div class="task-list" v-if="filter === 'favs'">
+      <p>You have {{ taskStore.favCount }} favs left to do</p>
+      <div v-for="task in taskStore.favs" :key="task.id">
         <TaskDetails :task="task"/>
       </div>
     </div>
@@ -16,13 +28,16 @@
 <script>
 import { useTaskStore } from './stores/TaskStore';
 import TaskDetails from './components/TaskDetails.vue';
+import { ref } from 'vue';
 export default {
   components: {
     TaskDetails
   },
   setup() {
     const taskStore = useTaskStore()
-    return { taskStore }
+
+    const filter = ref('all')
+    return { taskStore, filter }
   }
 }
 </script>
--- src/stores/TaskStore.js
+++ src/stores/TaskStore.js
@@ -3,9 +3,22 @@ import { defineStore } from "pinia";
 export const useTaskStore = defineStore('taskStore', {
     state: () => ({
         tasks: [
-            {id: 1, title: "buy some milk", isFav: false},
-            {id: 1, title: "play Hogwarts Legacy", isFav: true}
-        ], 
+            { id: 1, title: "buy some milk", isFav: false },
+            { id: 1, title: "play Hogwarts Legacy", isFav: true }
+        ],
         name: "ParvaCamer"
-    })
+    }),
+    getters: {
+        favs() {
+            return this.tasks.filter(task => task.isFav)
+        },
+        favCount() {
+            return this.tasks.reduce((previews, current) => {
+                return current.isFav ? previews + 1 : previews
+            }, 0)
+        },
+        totalCount: (state) => {
+            return state.tasks.length
+        }
+    }
 })
